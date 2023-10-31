95cfa6d7169b6ba7696760023d69102c4feb414f
vhjiang <scidotmoney@gmail.com>
2023-10-31 22:42:47 +0800

    add proveTask and readStatus

--- src/ProofPool.sol
+++ src/ProofPool.sol
@@ -6,62 +6,99 @@
 // /_/   \_\___/|_| \_/_/   \_|_|\_|___|  |_____/_/   \_|____|____/ 
 
 pragma solidity ^0.8.20;
-
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { IERC1271 } from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Counters} from "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import { LibBytesUtils } from "./libs/LibBytesUtils.sol";
 
-
-// submit task
-// submit proof
-// read proof data
 
 struct TaskAssignment {
     address prover;
     address feeToken;
-    address amount;
+    uint256 amount;
     uint64 expiry;
     bytes signature;
 }
 
 struct TaskStatus {
     bytes instance;
+    address prover;
+    uint64 submittedAt;
     bool proven;
 }
 
-
 contract ProofPool {
+    using Address for address;
 
-    using Counters for Counters.Counter;
 
+    bytes4 internal constant EIP1271_MAGICVALUE = 0x1626ba7e;
 
     address bondToken;
     uint256 bondAmount;
     address verifierAddress;
+    // The maximum time window allowed for a proof submission (in seconds).
+    uint32 proofWindow;
+    // The length of instance (in bytes).
+    uint8 instanceLength;
 
-    Counters.Counter public taskIdCounter;
-    mapping(uint256 => TaskStatus) public taskStatus;
+    mapping(bytes32 => TaskStatus) public taskStatusMap;
 
-    event TaskSubmitted();
+    event TaskSubmitted(
+        address indexed requester,
+        address indexed prover,
+        bytes32 taskKey,
+        address token,
+        uint256 amount
+    );
 
-    event BondDeposited();
+    event TaskProven(
+        address indexed prover,
+        bytes32 taskKey
+    );
+
+    event BondDeposited(
+        address indexed from,
+        bytes32 taskKey,
+        uint256 amount
+    );
+
+    event BondReleased(
+        address indexed to,
+        bytes32 taskKey,
+        uint256 amount
+    );
 
     error INVALID_ASSIGNMENT();
+    error INVALID_PROVER();
     error INVALID_PROVER_SIG();
+    error TASK_ALREADY_SUBMITTED();
+    error TASK_ALREADY_PROVEN();
+    error TASK_NONE_EXIST();
+    error TASK_NOT_THE_SAME();
+    error INVALID_PROOF();
+    error TASK_NOT_OPEN();
+    error TASK_ALREADY_OPEN();
 
+    // or config
+    // should be only owner
     function init(
-        address calldata _bondToken,
-        uint256 calldata _bondAmount,
-        address calldata _verifierAddress
-        
+        address  _bondToken,
+        uint256  _bondAmount,
+        address  _verifierAddress,
+        uint32  _proofWindow,
+        uint8  _instanceLength
     )
         external
     {   
         // only owner
         // init bond type and amout for this task
         // init verifier address
+        // init proof window
         bondToken = _bondToken;
         bondAmount = _bondAmount;
         verifierAddress = _verifierAddress;
+        proofWindow = _proofWindow;
+        instanceLength = _instanceLength;
     }
 
 
@@ -71,35 +108,46 @@ contract ProofPool {
         TaskAssignment memory assignment
     )
         external
-        returns (uint256 taskId)
+        returns (bytes32 taskKey)
     {
 
         // Check prover assignment
-        if (
-            assignment.expiry <= block.timestamp
-        ) {
+        if (assignment.expiry <= block.timestamp) {
             revert INVALID_ASSIGNMENT();
         }
 
-        // pay the reward
+        // Check task already submitted
+        taskKey = keccak256(instance);
+        if (taskStatusMap[taskKey].prover != address(0)) {
+            revert TASK_ALREADY_SUBMITTED();
+        }
+
+        // Pay the reward
         IERC20(assignment.feeToken).transferFrom(
             msg.sender,
             assignment.prover,
             assignment.amount
         );
 
-        // deposited bond
-        IERC20(bondAddress).transferFrom(
+        // Deposit the bond
+        IERC20(bondToken).transferFrom(
             assignment.prover,
-            this,
+            address(this),
             bondAmount
         );
 
+        emit BondDeposited(
+            assignment.prover,
+            taskKey,
+            bondAmount
+        );
+
+        // Check the signature
         if (!assignment.prover.isContract()) {
             address assignedProver = assignment.prover;
 
             if (
-                _hashAssignment(input, assignment).recover(assignment.data)
+                _hashAssignment(instance, assignment).recover(assignment.signature)
                     != assignedProver
             ) {
                 revert INVALID_PROVER_SIG();
@@ -110,7 +158,7 @@ contract ProofPool {
         ) {
             if (
                 IERC1271(assignment.prover).isValidSignature(
-                    _hashAssignment(input, assignment), assignment.data
+                    _hashAssignment(instance, assignment), assignment.signature
                 ) != EIP1271_MAGICVALUE
             ) {
                 revert INVALID_PROVER_SIG();
@@ -120,110 +168,108 @@ contract ProofPool {
             revert INVALID_PROVER();
         }
 
-        taskId = taskIdCounter.current();
-        taskStatus[taskId] = TaskStatus({
+        // Save the task status
+        taskStatusMap[taskKey] = TaskStatus({
             instance: instance,
-            status: false
+            prover: assignment.prover,
+            submittedAt: block.timestamp,
+            proven: false
         });
-        taskIdCounter.increment();
 
-
+        emit TaskSubmitted(
+            msg.sender,
+            assignment.prover,
+            taskKey,
+            assignment.feeToken,
+            assignment.amount
+        );
+    
     }
 
-    function submitProof(
-        bytes calldata input,
-        bytes calldata txList
+    function proveTask(
+        bytes calldata taskKey,
+        bytes calldata proof
     )
         external
-    {
+    {  
+        TaskStatus storage taskStatus = taskStatusMap[taskKey];
+        if (taskStatus.prover == address(0) && taskStatus.submittedAt == 0) {
+            revert TASK_NONE_EXIST();
+        }
 
+        if (
+            !LibBytesUtils.equal(
+                taskStatus.instance,
+                LibBytesUtils.slice(proof, 0, instanceLength)
+            )
+        ) {
+            revert TASK_NOT_THE_SAME();
+        }
+
+
+
+        if (taskStatus.submittedAt + proofWindow <= block.time && taskStatus.prover != msg.sender) {
+            revert TASK_NOT_OPEN();
+        } else if (taskStatus.submittedAt + proofWindow > block.time && taskStatus.prover == msg.sender) {
+            revert TASK_ALREADY_OPEN();
+        }
+
+        (bool _isCallSuccess, bytes memory _response) = verifierAddress.staticcall(proof);
+        if (!_isCallSuccess) {
+            revert INVALID_PROOF();
+        }
+
+        // Update status
+        taskStatus.proven = true;
+
+        // Release the bond
+        IERC20(bondToken).transfer(
+            msg.sender,
+            bondAmount
+        );
+
+        emit BondReleased(
+            msg.sender,
+            taskKey,
+            bondAmount
+        );
+
+        emit TaskProven(
+            msg.sender,
+            taskKey
+        );
+        
     }
 
     function readProofStatus(
-        bytes calldata input,
-        bytes calldata txList
+        bytes calldata taskKey
     )
         external
+        view
+        returns (TaskStatus memory taskStatus)
     {
-
-    }
-
-    function hashAssignmentForTxList(
-        TaikoData.ProverAssignment memory assignment,
-        bytes32 txListHash
-    )
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(
-            abi.encode(
-                "PROVER_ASSIGNMENT",
-                txListHash,
-                assignment.feeToken,
-                assignment.expiry,
-                assignment.tierFees
-            )
-        );
-    }
-
-    function _validateAssignment(
-        uint16 minTier,
-        bytes32 txListHash,
-        TaikoData.ProverAssignment memory assignment
-    )
-        private
-        returns (uint256 proverFee)
-    {
-        // Check assignment not expired
-        if (block.timestamp >= assignment.expiry) {
-            revert L1_ASSIGNMENT_EXPIRED();
-        }
-
-        if (txListHash == 0 || assignment.prover == address(0)) {
-            revert L1_ASSIGNMENT_INVALID_PARAMS();
-        }
-
-        // Hash the assignment with the txListHash, this hash will be signed by
-        // the prover, therefore, we add a string as a prefix.
-        bytes32 hash = hashAssignmentForTxList(assignment, txListHash);
-
-        if (!assignment.prover.isValidSignature(hash, assignment.signature)) {
-            revert L1_ASSIGNMENT_INVALID_SIG();
-        }
-
-        // Find the prover fee using the minimal tier
-        proverFee = _getProverFee(assignment.tierFees, minTier);
-
-        // The proposer irrevocably pays a fee to the assigned prover, either in
-        // Ether or ERC20 tokens.
-        if (assignment.feeToken == address(0)) {
-            // Paying Ether
-            if (msg.value < proverFee) revert L1_ASSIGNMENT_INSUFFICIENT_FEE();
-            assignment.prover.sendEther(proverFee);
-            unchecked {
-                // Return the extra Ether to the proposer
-                uint256 refund = msg.value - proverFee;
-                if (refund != 0) msg.sender.sendEther(refund);
-            }
-        } else {
-            // Paying ERC20 tokens
-            if (msg.value != 0) msg.sender.sendEther(msg.value);
-            ERC20Upgradeable(assignment.feeToken).transferFrom(
-                msg.sender, assignment.prover, proverFee
-            );
+        taskStatus = taskStatusMap[taskKey];
+        if (taskStatus.prover == address(0) && taskStatus.submittedAt == 0) {
+            revert TASK_NONE_EXIST();
         }
     }
 
     function _hashAssignment(
-        TaikoData.BlockMetadataInput memory input,
-        TaikoData.ProverAssignment memory assignment
+        bytes memory _instance,
+        TaskAssignment memory _assignment
     )
         private
         view
         returns (bytes32)
     {
-        return keccak256(abi.encode(input, msg.value, assignment.expiry));
+        return keccak256(
+            abi.encode(
+                _instance,
+                _assignment.feeToken,
+                _assignment.amout,
+                _assignment.expiry
+            )
+        );
     }
 
 }
